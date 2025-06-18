2bbbca288160cf803b3b340f7341571e66d430dc
Ankit Kumar <Ankit27.iitd@gmail.com>
2025-06-18 11:23:45 +0000

    completed entry.cpp

--- entry.cpp
+++ entry.cpp
@@ -1,3 +1,21 @@
 /* Do NOT add/remove any includes statements from this header file */
 /* unless EXPLICTLY clarified on Piazza. */
 #include "entry.h"
+
+SymEntry::Symentry() {
+    key = "";
+    val = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+SymEntry::SymEntry(string k, UnlimitedRational* v) {
+    key = k;
+    val = v;
+    left = nullptr;
+    right = nullptr;
+
+}
+SymEntry::~SymEntry() {
+    left = nullptr;
+    right = nullptr;
+}
